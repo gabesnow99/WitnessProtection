4013b22b2f54cd4787efaddadaf305696e158423
Gabe "Bendu" Snow <gabesnow99@gmail.com>
2025-08-01 13:59:08 -0600

    Initial setup of witness protection game/sim

---
+++ characters/player.py
@@ -0,0 +1,11 @@
+class Player():
+    def __init__(self):
+        self.speed = 5.
+        self.endurance = 5.
+        self.color = None
+
+    def tick():
+        pass
+
+    def move():
+        pass
